af59a51bbb7e58d9533d6e175857f5456f1ff671
Evan Shortiss <evanshortiss@gmail.com>
2019-10-11 12:53:38 -0700

    fix: improve error handling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path = require('path')
+const boom = require('@hapi/boom')
 const express = require('express')
 const probe = require('kube-probe')
 const helmet = require('helmet')
@@ -33,20 +34,53 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 applyKeycloak(app)
 
 // Render the order management page
-app.get('/', async (req, res) => {
-  const orders = await getOrders()
-  const username = req.kauth ? req.kauth.grant.access_token.content.preferred_username : undefined
+app.get('/', async (req, res, next) => {
+  try {
+    const orders = await getOrders()
+    const username = req.kauth ? req.kauth.grant.access_token.content.preferred_username : undefined
 
-  // Change the date property in the order object
-  // from a number to formatted string
-  orders.forEach(o => {
-    o.datetime = new Date(o.datetime).toJSON()
-  })
+    // Change the date property in the order object
+    // from a number to formatted string
+    orders.forEach(o => {
+      o.datetime = new Date(o.datetime).toJSON()
+    })
 
-  res.render('index.handlebars', {
-    orders,
-    username
+    res.render('index.handlebars', {
+      orders,
+      username
+    })
+  } catch (e) {
+    next(e)
+  }
+})
+
+// 404 handler. If a request gets here it matched no routes
+app.use((req, res) => {
+  res.render('error.handlebars', {
+    title: '404 - Not Found',
+    desciption: 'Looks like you entered an invalid URL or followed a broken link. Use the button below to get back to the application.'
   })
+})
+
+app.use((err, req, res, next) => {
+  log.error('server error', err)
+  log.error(err.stack)
+
+  const description = 'Sorry, we had some trouble processing your request. Try again later.'
+
+  if (boom.isBoom(err)) {
+    log.error('rendering boom error')
+    res.render('error.handlebars', {
+      title: `${err.output.statusCode} - ${err.output.payload.error}`,
+      description: err.output.statusCode === 500 ? description : err.output.payload.message
+    })
+  } else {
+    log.error('rendering regular error')
+    res.render('error.handlebars', {
+      title: 'Unknown Error',
+      description
+    })
+  }
 })
 
 app.listen(HTTP_PORT, (err) => {
